74dd4c4ed5e8c11766643f6be37722a2e4de551d
szymonH <szymonH4679@gmail.com>
2024-12-09 11:32:44 +0100

    finished interface, coin and dice class

--- app/src/main/java/com/example/trudnezadaniazkotlina/CoinDiceRollSimulator.kt
+++ app/src/main/java/com/example/trudnezadaniazkotlina/CoinDiceRollSimulator.kt
@@ -1,2 +1,36 @@
 package com.example.trudnezadaniazkotlina
 
+interface RollingTool{
+    fun roll()
+}
+
+class Coin : RollingTool{
+
+    override fun roll(){
+        val coinThrow: Int = (1..2).random()
+        if (coinThrow == 1){
+            println("Rzut monetą: orzeł")
+        }
+        else{
+            println("Rzut monetą: reszka")
+        }
+    }
+
+}
+
+class Dice : RollingTool{
+    override fun roll(){
+        val diceRoll: Int = (1..6).random()
+        println("Twoja kostka wylosowała: $diceRoll")
+    }
+}
+
+fun main(){
+    while (true){
+        println("Rzut monetą: C, Rzut kostką: D, Zakończenie programu: E: ")
+        val dzialanie: String = readln()
+        println(dzialanie)
+        break
+
+    }
+}
