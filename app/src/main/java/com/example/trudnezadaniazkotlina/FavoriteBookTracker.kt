1dfa7baff82f7bf84d5c330fde2c8ec33b03a1c7
szymonH <szymonH4679@gmail.com>
2024-12-16 11:22:37 +0100

    Finished Favorite Book Tracker application

--- app/src/main/java/com/example/trudnezadaniazkotlina/FavoriteBookTracker.kt
+++ app/src/main/java/com/example/trudnezadaniazkotlina/FavoriteBookTracker.kt
@@ -11,8 +11,8 @@ class Book(val author : String, val year : Int, val title : String){
 object FavoriteBooks{
     private val bookList = mutableListOf<Book>()
 
-    fun addBook(inputAuthor : String, inputYear: Int, inputTitle: String){
-        bookList.add(Book(inputAuthor, inputYear, inputTitle))
+    fun addBook(inputAuthor : String, inputYear: String, inputTitle: String){
+        bookList.add(Book(inputAuthor, inputYear.toInt(), inputTitle))
     }
 
     fun filterBooks(option : String){
@@ -22,14 +22,14 @@ object FavoriteBooks{
                 print("podaj imie autora jakiego książki chesz wyświetlić: ")
                 val authorName : String = readln()
                 val filteredBooks = bookList.filter { it.author == authorName}
-                println(filteredBooks)
+                filteredBooks.forEach { it -> println("Autor: ${it.author}, tytuł: ${it.title}, rok: ${it.year}") }
             }
             "year" -> {
                 print("podaj rok z jakiego książki chesz wyświetlić: ")
                 val inputBookYear : String = readln()
                 val bookYear : Int = inputBookYear.toInt()
                 val filteredBooks = bookList.filter { it.year== bookYear}
-                println(filteredBooks)
+                filteredBooks.forEach { it -> println("Autor: ${it.author}, tytuł: ${it.title}, rok: ${it.year}") }
             }
             else -> {
                 println("Brak wyboru filtrownia")
@@ -40,10 +40,53 @@ object FavoriteBooks{
 
     fun sortBooks(option: String){
 
+        when (option)
+        {
+            "title" -> bookList.sortedBy { it.author }.forEach{ it -> println("Autor: ${it.author}, tytuł: ${it.title}, rok: ${it.year}") }
+
+                "all" -> bookList.forEach { it -> println("Autor: ${it.author}, tytuł: ${it.title}, rok: ${it.year}") }
+
+            else -> println("Nie wybrano metody sortowania")
+        }
     }
 }
 
 
 fun main(){
+
+
+    while (true)
+    {
+        println("Dodaj książkę: add, Filtruj książki: filter, Sortuj książki: sort, Zakończ program: end")
+        val choice : String = readln()
+        when (choice) {
+            "add" -> {
+                print("Podaj imie autora: ")
+                val inputAuthor : String = readln()
+                print("Podaj tytuł: ")
+                val inputTitle : String = readln()
+                print("Podaj rok wydania: ")
+                val inputYear : String = readln()
+                FavoriteBooks.addBook(inputAuthor, inputYear, inputTitle)
+
+            }
+
+
+            "filter" -> {
+                println("Filtruj po autorze: author, Filtruj po roku wydania: year")
+                val option : String = readln()
+                FavoriteBooks.filterBooks(option)
+
+            }
+            "sort" -> {
+                println("Sortuj po tytule: title, Wyświetl wszytskie: all")
+                val option : String = readln()
+                FavoriteBooks.sortBooks(option)
+
+            }
+            else -> break
+
+        }
+    }
 
 }
